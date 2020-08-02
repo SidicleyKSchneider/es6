9ade4a63cf29eadba6b62228d5c5bd9758e247be
Sidicley Schneider <sidi.fev@gmail.com>
2020-08-02 20:20:58 -0300

    arrows functions

--- bundle.js
+++ bundle.js
@@ -2,33 +2,19 @@
 
 var arr = [1, 2, 3, 4, 5, 6];
 var cesta = ["banana", "maçã", "abacate", "abacaxi"];
-var squareArr = arr.map(function (item, index) {
+var squareArr = arr.map(function (item) {
   return item * item;
-});
-var fruitPosition = cesta.map(function (item, index) {
-  return "A/O ".concat(item, " est\xE1 na posi\xE7\xE3o ").concat(index, " do array");
 }); //console.log(squareArr);
-//console.log(fruitPosition);
 
-var sum = arr.reduce(function (total, next) {
-  //console.log("total ", total);
-  //console.log("next ", next);
-  return total + next;
-}); //console.log(sum);
+var teste = function teste() {
+  return [2, 5, 8, 4];
+};
 
-var filter = arr.filter(function (item) {
-  return item % 2 === 0;
-});
-var filter2 = cesta.filter(function (item) {
-  return item === "banana";
-}); //console.log(filter);
-//console.log(filter2);
+var teste2 = function teste2() {
+  return {
+    nome: "Sidicley"
+  };
+};
 
-var find = arr.find(function (item) {
-  return item === 3;
-});
-var find2 = cesta.find(function (item) {
-  return item === "maçã";
-});
-console.log(find);
-console.log(find2);
+console.log(teste());
+console.log(teste2());
--- main.js
+++ main.js
@@ -1,42 +1,12 @@
 const arr = [1, 2, 3, 4, 5, 6];
 const cesta = ["banana", "maçã", "abacate", "abacaxi"];
 
-const squareArr = arr.map(function (item, index) {
-  return item * item;
-})
-
-const fruitPosition = cesta.map(function (item, index) {
-  return `A/O ${item} está na posição ${index} do array`;
-});
+const squareArr = arr.map(item => item * item);
 
 //console.log(squareArr);
-//console.log(fruitPosition);
 
-const sum = arr.reduce(function (total, next) {
-  //console.log("total ", total);
-  //console.log("next ", next);
-  return total + next;
-});
+const teste = () => [2, 5, 8, 4];
+const teste2 = () => ({ nome: "Sidicley" });
 
-//console.log(sum);
-
-const filter = arr.filter(function (item) {
-  return item % 2 === 0;
-});
-
-const filter2 = cesta.filter(function (item) {
-  return item === "banana";
-});
-
-//console.log(filter);
-//console.log(filter2);
-
-const find = arr.find(function (item) {
-  return item === 3;
-});
-const find2 = cesta.find(function (item) {
-  return item === "maçã";
-});
-
-console.log(find);
-console.log(find2);
+console.log(teste());
+console.log(teste2());
